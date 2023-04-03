3e623f78e905c6367b7a2d0d1d2bad75c2e8ae3c
Austin Small <austinsmall@MacBook-Pro-2.dpsnc.local>
2023-04-03 15:53:17 -0400

    locations

--- src/components/Locations/index.js
+++ src/components/Locations/index.js
@@ -2,124 +2,183 @@ import React from 'react';
 
 function TextColumns() {
   return (
-      <div className="columns-3 container p-6">
-                    <div> 
-                        <h2 id="#a" className="py-3">A</h2>
-                    </div>
-                    <div>
-                        <h2 className="py-1.5">B</h2>
-                        <ul>
-                            <li> Biltmore Hotel </li>
-                            <li>Biltmore Service Station</li>
-                            <li>Boykin Tailors</li>
-                            <li>Bull City Drug Store </li>
-                            
-                        </ul>
-                    </div>
-                    <div className="py-5">
-                        <h2 className="py-1.5">C</h2>
-                        <ol className="space-y-.5">
-                            <li>Catlett's Restaurant </li>
-                            <li>Chautauqua Tavern</li>
-                            <li>Clay's Service Station </li>
-                            <li>College Inn Restaurant/Tavern </li>
-                            <li>Community Tailors</li>
-                            <li>Congro Grill</li>
-                            <li>Cu-Cu Restaurant (Justin Wright)</li>
-                        </ol>
-                    </div>
+    <div className="container mx-full columns-4 p-6 flex-wrap">
+        <div> 
+            <h2 className="py-3">A</h2>
+        </div>
 
-                    <div className="py-5"> 
-                    <h2 className="py-1.5">D</h2>
-                    <ol className="space-y-.5">
-                        <li>De Lux Barber Shop </li>
-                        <li>De Shazors Hostelry/Beauty Parlor  </li>
-                        <li>D'Orsay Beauty Parlor </li>
-                    </ol>
-                    </div>
+        <div>
+            <h2 className="py-1.5">B</h2>
+             <ul>
+                <li> Biltmore Hotel </li>
+                <li>Biltmore Service Station</li>
+                <li>Boykin Tailors</li>
+                <li>Bull City Drug Store </li>     
+            </ul>
+        </div>
 
-                    <div className="py-5">
-                        <h2 className="py-1.5">E</h2>
-                        <ol className="space-y-.5">
-                            <li>Elite</li>
-                        </ol>
-                    </div>
+        <div className="py-5">
+            <h2 className="py-1.5">C</h2>
+                <ol className="space-y-.5">
+                 <li>Catlett's Restaurant </li>
+                 <li>Chautauqua Tavern</li>
+                 <li>Clay's Service Station </li>
+                 <li>College Inn Restaurant/Tavern </li>
+                 <li>Community Tailors</li>
+                 <li>Congro Grill</li>
+                 <li>Cu-Cu Restaurant (Justin Wright)</li>
+                </ol>
+        </div>
 
-                    <div>
-                        <h2 className="py-1.5"> F</h2>
-                        <ol>
-                            <li>Friendly City Beauty Parlor/Friendly Barber Shop </li>
+        <div className="py-5"> 
+            <h2 className="py-1.5">D</h2>
+                 <ol className="space-y-.5">
+                    <li>De Lux Barber Shop </li>
+                    <li>De Shazors Hostelry/Beauty Parlor  </li>
+                    <li>D'Orsay Beauty Parlor </li>
+                </ol>
+        </div>
 
-                        </ol>
-                    </div>
+        <div className="py-5">
+            <h2 className="py-1.5">E</h2>
+            <ol className="space-y-.5">
+                <li>Elite</li>
+            </ol>
+        </div>
 
-                    <div>
-                        <h2 className="py-1.5">G</h2>
-                        <ol className="space-y-.5">
-                            <li>Garrett's Biltmore Drug Store</li>
-                            <li>Granite Service Station</li>
-                        </ol>
-                    </div>
+        <div className='py-5'>
+            <h2 className="py-1.5"> F</h2>
+            <ol className='space-y-.5'>
+                <li>Friendly City Beauty Parlor/Friendly Barber Shop </li>
 
-                    <div>
-                        <h2 className="py-1.5">H</h2>
-                        <ol>
-                            <li>
-                            Hollywood Tavern (Gian Hasbrock)
-                            </li>
-                        </ol>
-                    </div>
+            </ol>
+        </div>
 
-                    <div>
-                        <h2>I</h2>
-                    </div>
+        <div className='py-5'>
+            <h2 className="py-1.5">G</h2>
+            <ol className="space-y-.5">
+                <li>Garrett's Biltmore Drug Store</li>
+                <li>Granite Service Station</li>
+            </ol>
+        </div>
 
-                    <div>
-                        <h2>J</h2>
-                        <ol>
-                            <li>Jack's Grill Tavern </li>
-                            <li>Jones Hotel </li>
-                        </ol>
-                    </div>
+        <div className='py-5'>
+            <h2 className="py-1.5">H</h2>
+            <ol className='space-y-.5'>
+                <li>Hollywood Tavern</li>
+            </ol>
+        </div>
 
-                    <div>
-                        <h2>K</h2>
-                    </div>
+        <div className='py-5'>
+            <h2 className='py-1.5'>I</h2>
+        </div>
 
-                    <div>
-                        <h2>L</h2>
-                    </div>
+        <div className='py-5'>
+            <h2 className='py-1.5'>J</h2>
+            <ol className='space-y-.5'>
+                <li>Jack's Grill Tavern </li>
+                <li>Jones Hotel </li>
+            </ol>
+        </div>
 
-                    <div>
-                        <h2>M</h2>
-                        <ol>
-                            <li>Midway Service Station</li>
-                            <li>Mrs. S. A. Morris </li>
-                            <li>Mrs. Mary Sims </li>
-                            <li>Mrs. N. O'Daniel </li>
-                        </ol>
-                    </div>
+        <div className='py-5'>
+            <h2 className='py-1.5'>K</h2>
+        </div>
 
-                    <div>
-                        <h2>N</h2>
-                    </div>
+        <div className='py-5'>
+            <h2 className='py-1.5'> L</h2>
+        </div>
 
-                    <div>
-                        <h2>O</h2>
-                    </div>
+        <div className='py-5'>
+            <h2 className='py-1.5'>M</h2>
+            <ol className='space-y-.5'>
+                <li>Midway Service Station</li>
+                <li>Mrs. S. A. Morris </li>
+                <li>Mrs. Mary Sims </li>
+                 <li>Mrs. N. O'Daniel </li>
+            </ol>
+        </div>
 
-                    <div>
-                        <h2>P</h2>
-                    </div>
-                        <div>
-                            <ol>
-                                <li>Parker's Inn Tavern </li>
-                                <li>Pettigrew Street</li>
-                                <li>Pine Street Service Station</li>
-                            </ol>
-                        </div>
+        <div className='py-5'>
+            <h2 className='py-1.5'>N</h2>
+        </div>
 
-                </div>
+        <div className='py-5'>
+            <h2 className='py-1.5'>O</h2>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>P</h2>
+                <ol className='space-y-.5'>
+                    <li>Parker's Inn Tavern </li>
+                    <li>Pettigrew Street</li>
+                    <li>Pine Street Service Station</li>
+                </ol>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>Q</h2>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>R</h2>
+            <ol className='space-y-.5'>
+                <li>Royal's Cleaners/Tailor </li>
+            </ol>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>S</h2>
+            <ol className='space-y-.5'>
+                <li>Scott & Roberts Tailors</li>
+                <li>Service Tailors (Miya Vargas)</li>
+                <li>Speight's Service Station</li>
+                <li>Sulton's Esso Service Station</li>
+            </ol>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>T</h2>
+            <ol className='space-y-.5'>
+                <li>Tavrens Shaws Cafe </li>
+                <li>Thompson's Esso Servicecenter</li>
+            </ol>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>U</h2>
+            <ol className='space-y-.5'>
+                <li>Union Tailors</li>
+            </ol>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>V</h2>
+            <ol className='space-y-.5'>
+                <li>Vanity Fair Beauty Parlor </li>
+            </ol>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>W</h2>
+            <ol className='space-y-.5'>
+                <li>William's Service Station </li>
+            </ol>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>X</h2>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>Y</h2>
+        </div>
+
+        <div className='py-5'>
+            <h2 className='py-1.5'>Z</h2>
+        </div>
+
+    </div>
   );
 }
 
