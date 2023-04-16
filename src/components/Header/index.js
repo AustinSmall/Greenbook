2533eaba7b72698245001568a7c112b64a8ab501
Austin Small <austinsmall@MacBook-Pro-2.lan>
2023-04-16 15:01:47 -0400

    header responsiveness fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,27 +3,23 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header className="bg-green-800 md:sticky top-0 z-10">
+      <header className="bg-green-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="/" className="ml-3 text-xl">
-            Greenbook
+            GreenBook
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="/about" className="mr-5 hover:text-white">
-          </a>
           <a href="/locations" className="mr-5 hover:text-white">
             Locations
           </a>
-        <a
-          href="/timeline"
-          className="inline-flex items-center  border-0 py-1  focus:outline-none hover:text-white rounded text-base mt-4 md:mt-0">
-          Timeline
+          <a href="/timeline" className="mr-5 hover:text-white">
+            Timeline
           </a>
         </nav>
-          </div>
-          </header>
+      </div>
+    </header>
 
     )
 }
