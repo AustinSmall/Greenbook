54ba4cdebe07e00c8b5d3905b7bab6e7463d0d18
Austin Small <austinsmall@MacBook-Pro-2.dpsnc.local>
2023-04-17 15:03:32 -0400

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
          <Slideshowcomponent/>
     </div>
     <div>
-        <h1>Our Purpose</h1>
+        <h1 className='text-center pt-2'>Our Purpose</h1>
     </div>
      <section className='text-center'>
         <p>
@@ -16,13 +16,15 @@ const Home = () => {
         Listings peaked between 1947–1955 and were overwhelmingly concentrated in the cultural center of the Black population, the neighborhood known as Hayti. 
         </p>
 
-        <h1>Our Story</h1>
+        <h1 className='text-center pt-4'>Our Story</h1>
         <p>
-          Our creators realize that nonprofit organizations are paying large amounts of money to have interactive portals 
-          that allow them to communicate with their donors and future donors. This site will bridge the gap between donors and
-          account managers of nonprofit organizations.
+          Minim anim eiusmod aute dolore Lorem in deserunt minim eiusmod. Irure eu do eiusmod mollit irure velit veniam adipisicing enim cillum aliqua ea esse nisi.
+          Ad irure occaecat qui cillum officia cupidatat ea adipisicing labore quis.
+          Velit ut enim ipsum quis consequat. Esse est aute labore enim nisi.
+          Culpa veniam nisi adipisicing minim aute cillum et sunt reprehenderit ea officia.
+          Nulla dolore eu deserunt excepteur fugiat tempor ut aute.
         </p>
-        <h1>How It All Works</h1>
+        <footer className='bg-vinatge'></footer>
         
     </section>
         </main>
